f09deeda3bcfe51ec77f94295f46b8fe35a64f7d
Maria SAKOVYCH <msakovyc@e3r4p2.unit.ua>
2018-08-15 20:09:16 +0300

    find solutions

--- main.c
+++ main.c
@@ -54,11 +54,12 @@ void	read_map(t_lemin *ptr)
 	{
 		if (!ft_strcmp(line, "##start") || !ft_strcmp(line, "##end"))
 		{
-			if (ft_strcmp(line, "##start"))
+			if (!ft_strcmp(line, "##start"))
 				ptr->start = ptr->count_r;
 			else
 				ptr->end = ptr->count_r;
 			ft_printf("%s\n", line);
+			ft_printf("start = %d   end = %d\n", ptr->start, ptr->end);
 			ft_strdel(&line);
 			while (GNL(0, &line) > 0 && ft_strchr(line, '#'))
 			{
@@ -79,33 +80,35 @@ void	read_map(t_lemin *ptr)
 	ft_printf(BLUE "END WRITTNG\n" NC);
 }
 
-int		check_link(int i, int j, t_lemin *ptr)
+int		check_link(int n, int k, t_lemin *ptr)
 {
-	if (ptr->links[i][j] == 0)
-		return (0);
-	ft_printf("%s-", namebyi(ptr, i));
-	ptr->links[i][j] = 0;
-	ptr->links[j][i] = 0;
-	if (i == ptr->end)
+	if (n == ptr->end)
+	{
+		ft_printf("%s\n", namebyi(ptr, n));
 		ft_printf("END\n");
-	// if (ptr->start + 1 < ptr->count_r)
-	// 	check_link(ptr->start, ptr->start + 1, ptr);
-	// if (ptr->start - 1 < ptr->count_r)
-	// 	check_link(ptr->start, ptr->start - 1, ptr);
+		return (0);
+	}
+	if (ptr->links[n][k] == 0)
+		return (0);
+	ft_printf("%s -> ", namebyi(ptr, n));
+	ptr->links[n][k] = 0;
+	ptr->links[k][n] = 0;
+	int i = 0;
+	while (i < ptr->count_r)
+		check_link(k, i++, ptr);
 	return (1);
 }
 
-int		find_solutions(t_lemin *ptr)
+int		find_solutions(t_lemin *ptr, int size)
 {
 	int			i;
-	// t_hashmap	*ways;
+	t_hashmap	*ways[size];
 
-	i = 1;
-	while (ptr->start + i < ptr->count_r)
-		check_link(ptr->start, ptr->start + i++, ptr);
-	// i = 1;
-	// while (ptr->start - i < ptr->count_r)
-	// 	check_link(ptr->start, ptr->start - i++, ptr);
+	i = 0;
+	ways[0] = ptr->rooms;
+	while (i < ptr->count_r)
+		if (check_link(ptr->start, i++, ptr))
+			ft_printf("ok\n");
 	return (1);
 }
 
@@ -114,13 +117,14 @@ int		main(void)
 	t_lemin	*ptr;
 	char	*line;
 
+	// int fd = open("~/lem-in/map.txt", O_RDONLY);
 	if (!(ptr = malloc(sizeof(t_lemin))))
 		return (0);
 	ptr->l = 0;
 	ptr->count_r = 0;
 	ptr->start = -1;
 	ptr->end = -1;
-	while (GNL(0, &line) > 0 && !(ptr->ants = ft_atoi(line)))
+	while (GNL(0, &line) > 0 && ft_printf("%s\n", line) && !(ptr->ants = ft_atoi(line)))
 		ft_strdel(&line);
 	if (!line || !is_valid(line, 0) || !ptr->ants)
 		error(1, line);
@@ -136,7 +140,13 @@ int		main(void)
 			ft_printf("%d ", ptr->links[j][k]);
 		ft_printf("\n");
 	}
-	find_solutions(ptr);
+	int i = 0;
+	int size = 0;
+	while (i < ptr->count_r)
+		if (ptr->links[ptr->start][i++] == 1)
+			size++;
+	ft_printf("size = %d\n", size);
+	find_solutions(ptr, size);
 	// while (1);
 	return (0);
 }
